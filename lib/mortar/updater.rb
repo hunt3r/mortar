3aa3efcc8a364fbab8f634eb51fbeb96d2a8d46a
Chris Ngan <chris.ngan24@gmail.com>
2014-02-13 13:08:44 -0500

    Added message to update with version:upgrade if installed with omnibus

--- lib/mortar/updater.rb
+++ lib/mortar/updater.rb
@@ -45,7 +45,11 @@ module Mortar
       newest_version = self.get_newest_version
 
       if compare_versions(newest_version, local_version) > 0
-        Mortar::Helpers.warning("There is a new version of the Mortar development framework available.  Please run 'gem install mortar' to install the latest version.\n\n")
+        if File.directory? ('/opt/mortar')
+          Mortar::Helpers.warning("There is a new version of the Mortar development framework available.  Please run 'mortar version:upgrade' to install the latest version.\n\n")
+        else
+          Mortar::Helpers.warning("There is a new version of the Mortar development framework available.  Please run 'gem install mortar' to install the latest version.\n\n")
+        end
       end
     end
   end
