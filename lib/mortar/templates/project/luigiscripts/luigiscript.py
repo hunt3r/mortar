7e2d26860061cec5377985bd406c1ca7be0814c8
Gurneet Arora <garora@mortars-MacBook-Air.local>
2014-10-03 13:29:04 -0400

    fixing comment

--- lib/mortar/templates/project/luigiscripts/luigiscript.py
+++ lib/mortar/templates/project/luigiscripts/luigiscript.py
@@ -24,7 +24,7 @@ from mortar.luigi import mortartask
 
   To Run:
     mortar local:luigi luigiscripts/<%= project_name_alias %>_luigi.py
-        -p output-base-path=s3://your/output_base_path
+        --p output-base-path=s3://your/output_base_path
 """
 
 MORTAR_PROJECT = <%= project_name_alias %>
